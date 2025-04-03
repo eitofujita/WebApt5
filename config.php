a1494a8a84279603f9313377ca7e4abb1f31ccc7
Eito <fifatvgame@gmail.com>
2025-04-04 04:15:08 +0900

    auto update

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define('DSN', 'mysql:host=localhost;dbname=webapt5;charset=utf8');
 define('DB_USER', 'root');
 define('DB_PASS', 'eito0226'); // ↑ で設定したもの
 
-$pdo = new PDO('mysql:host=localhost;dbname=webapt5;charset=utf8','root','');
+$pdo = new PDO('mysql:host=localhost;dbname=webapt5;charset=utf8','root','eito0226');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
